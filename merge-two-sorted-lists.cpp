1bfe001ac2f93fae710c19e6c8e95ac975132a3f
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-02 01:20:43 -0700

    added print linked list

--- merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists.cpp
@@ -2,6 +2,31 @@
 
 using namespace std;
 
-int main(int argc, char const *argv[]) {
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
+
+int main(int argc, char const* argv[]) {
   return 0;
+}
+
+ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+}
+
+// Prints Linked List
+// e.g. 5->8->3->NULL
+void printLinkedList(ListNode* head) {
+  ListNode* node = head;
+
+  while (node != nullptr) {
+    cout << node->val << "->";
+    node = node->next;
+  }
+  cout << "NULL\n";
 }
