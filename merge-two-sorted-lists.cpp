f143bbcea61e8140b4662aaa4529f4835fb21161
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-02 01:35:18 -0700

    added function to create linked list

--- merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
@@ -10,13 +11,36 @@ struct ListNode {
   ListNode(int x, ListNode* next) : val(x), next(next) {}
 };
 
+ListNode* createLinkedList(vector<int> values);
+
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
+void printLinkedList(ListNode* head);
 
 int main(int argc, char const* argv[]) {
+  vector<int> v = {5, 3, 7, 6};
+  ListNode* list1 = createLinkedList(v);
+  printLinkedList(list1);
   return 0;
 }
 
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+  ListNode* head;
+
+  return head;
+}
+
+// Create Linked list given a vector of int values
+// returns pointer to head of lined list
+ListNode* createLinkedList(vector<int> values) {
+  ListNode* head;
+  ListNode* node = new ListNode();
+  head = node;
+  for (int value : values) {
+    node->next = new ListNode(value);
+    node = node->next;
+  }
+  head = head->next;
+  return head;
 }
 
 // Prints Linked List
