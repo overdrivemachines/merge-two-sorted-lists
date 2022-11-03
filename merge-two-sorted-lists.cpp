c6322b0b66b8949424d08f70158ce53c392c0f5c
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-02 20:04:34 -0700

    finding bug

--- merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists.cpp
@@ -17,14 +17,54 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
 void printLinkedList(ListNode* head);
 
 int main(int argc, char const* argv[]) {
-  vector<int> v = {5, 3, 7, 6};
-  ListNode* list1 = createLinkedList(v);
+  vector<int> v1 = {3, 6, 7, 9};
+  vector<int> v2 = {2, 9, 10};
+  ListNode* list1 = createLinkedList(v1);
+  ListNode* list2 = createLinkedList(v2);
+  ListNode* merge_head;
+
   printLinkedList(list1);
+  printLinkedList(list2);
+
+  merge_head = mergeTwoLists(list1, list2);
+  printLinkedList(merge_head);
+
   return 0;
 }
 
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-  ListNode* head;
+  ListNode *head, *node;
+  head = list1;
+
+  if (list1 == nullptr)
+    return list2;
+  if (list2 == nullptr)
+    return list1;
+
+  // head is pointing to the list1 or list2 whichever one has lower value
+  head = (list1->val < list2->val) ? list1 : list2;
+  node = head;
+
+  cout << "merged list: " << node->val << "->";
+
+  while ((list1->next != nullptr) && (list2->next != nullptr)) {
+    if (list1->next->val < list2->next->val) {
+      node->next = list1->next;
+      list1 = list1->next;
+      cout << list1->val << "(1)->";
+    } else {
+      node->next = list2->next;
+      list2 = list2->next;
+      cout << list2->val << "(2)->";
+    }
+    node = node->next;
+  }
+
+  if (list1 != nullptr) {
+    node->next = nullptr;
+  } else if (list2 != nullptr) {
+    node->next = nullptr;
+  }
 
   return head;
 }
