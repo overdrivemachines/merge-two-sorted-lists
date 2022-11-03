1105b065693944c3947a083519044c44ba509cdb
get.dipen@gmail.com <get.dipen@gmail.com>
2022-11-02 21:50:52 -0700

    finished merging

--- merge-two-sorted-lists.cpp
+++ merge-two-sorted-lists.cpp
@@ -17,8 +17,9 @@ ListNode* mergeTwoLists(ListNode* list1, ListNode* list2);
 void printLinkedList(ListNode* head);
 
 int main(int argc, char const* argv[]) {
-  vector<int> v1 = {3, 6, 7, 9};
-  vector<int> v2 = {2, 9, 10};
+  vector<int> v1 = {3, 6, 7, 9, 9, 9, 9, 10, 11, 12};
+  vector<int> v2 = {2, 9, 10, 11};
+  // creating linked lists
   ListNode* list1 = createLinkedList(v1);
   ListNode* list2 = createLinkedList(v2);
   ListNode* merge_head;
@@ -26,45 +27,63 @@ int main(int argc, char const* argv[]) {
   printLinkedList(list1);
   printLinkedList(list2);
 
+  // merging the 2 linked lists
   merge_head = mergeTwoLists(list1, list2);
   printLinkedList(merge_head);
 
   return 0;
 }
 
+// merges list1 and list2
+// merged linked list is also sorted
 ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
   ListNode *head, *node;
   head = list1;
 
+  // if list1 is empty, return pointer to list2
   if (list1 == nullptr)
     return list2;
+
+  // if list2 is empty, return pointer to list1
   if (list2 == nullptr)
     return list1;
 
   // head is pointing to the list1 or list2 whichever one has lower value
-  head = (list1->val < list2->val) ? list1 : list2;
+  if (list1->val < list2->val) {
+    head = list1;
+    list1 = list1->next;
+  } else {
+    head = list2;
+    list2 = list2->next;
+  }
+
   node = head;
 
-  cout << "merged list: " << node->val << "->";
+  // cout << "head val: " << head->val << endl;
 
-  while ((list1->next != nullptr) && (list2->next != nullptr)) {
-    if (list1->next->val < list2->next->val) {
-      node->next = list1->next;
+  // cout << "merged list: " << node->val << "->";
+
+  while ((list1 != nullptr) && (list2 != nullptr)) {
+    if (list1->val < list2->val) {
+      node->next = list1;
       list1 = list1->next;
-      cout << list1->val << "(1)->";
     } else {
-      node->next = list2->next;
+      node->next = list2;
       list2 = list2->next;
-      cout << list2->val << "(2)->";
     }
     node = node->next;
+    // cout << node->val << "->";
   }
 
   if (list1 != nullptr) {
-    node->next = nullptr;
+    node->next = list1;
+    // cout << node->next->val << ":rest1";
   } else if (list2 != nullptr) {
-    node->next = nullptr;
+    node->next = list2;
+    // cout << node->next->val << ":rest2";
   }
+
+  // cout << endl;
 
   return head;
 }
